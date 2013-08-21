598b298e607dd3fcf4cec70f9e2839f4f0d598e9
eghik <eghik13@gmail.com>
2013-08-21 15:16:12 +0400

    new task

--- src/com/javarush/test/level06/lesson11/home05/Solution.java
+++ src/com/javarush/test/level06/lesson11/home05/Solution.java
@@ -16,15 +16,14 @@ public class Solution
 
     public static void printIdea (Idea idea)
     {
-       System.out.println(idea);
+        System.out.println(idea.getDescription());
     }
 
     public static class Idea
     {
-        public String getDescription()
+        public  String getDescription()
         {
-            String idea = null;
-            return idea.toString() ;
+            return "idea";
         }
     }
 }
